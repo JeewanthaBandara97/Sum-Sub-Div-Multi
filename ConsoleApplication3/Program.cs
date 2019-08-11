f476fdb439813fb6f0eab6f2f3ce96fab0a54136
Jeewantha Bandara <51124088+JeewanthaBandara97@users.noreply.github.com>
2019-08-11 17:49:09 +0530

    Add files via upload

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Number 1:");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Number 2:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("+ is" + (num1 + num2));
+            Console.WriteLine("- is" + (num1 - num2));
+            Console.WriteLine("* is" + (num1 * num2));
+            Console.WriteLine("/ is" + (num1 / num2));
+            
+            Console.WriteLine("/ is" + (double)(num1) / (double)(num2));
+
+            Console.ReadKey();
+        }
+    }
+}
